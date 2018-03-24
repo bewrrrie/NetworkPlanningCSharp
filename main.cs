0de61812cd8ccb3297d18db1bc67d619bf64790c
bewrrrie <silverr41@gmail.com>
2018-03-25 00:47:05 +0600

    add Topological sort, CPM class, GetCriticalProcesses method.

--- main.cs
+++ main.cs
@@ -4,19 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 
 
-struct Edge {
+// Be careful! Algorithms does not allow you to use directed graphs with cycles in them!
+
+
+struct Arrow {
 	private int begin;
 	private int end;
-	private int weight;
+	private double weight;
 
-	public Edge(int begin, int end, int weight)
+	public Arrow(Arrow e)
+	{
+		this.begin = e.begin;
+		this.end = e.end;
+		this.weight = e.weight;
+	}
+
+	public Arrow(int begin, int end, double weight)
 	{
 		this.begin = begin;
 		this.end = end;
 		this.weight = weight;
 	}
 
-	public Edge(int begin, int end)
+	public Arrow(int begin, int end)
 	{
 		this.begin = begin;
 		this.end = end;
@@ -33,7 +43,7 @@ struct Edge {
 		return end;
 	}
 
-	public int GetWeight()
+	public double GetWeight()
 	{
 		return weight;
 	}
@@ -45,11 +55,11 @@ struct Edge {
 
 	public override bool Equals(object obj) 
 	{
-		if (!(obj is Edge))
+		if (!(obj is Arrow))
 			return false;
 
-		Edge e = (Edge) obj;
-		return e.begin == this.begin && e.end == this.end;
+		Arrow a = (Arrow) obj;
+		return a.begin == this.begin && a.end == this.end;
 	}
 
 	public override int GetHashCode()
@@ -61,39 +71,101 @@ struct Edge {
 
 struct WeightedDiGraph
 {
-	private List<Edge> edges;
+	private List<Arrow> arrows;
 
-	public WeightedDiGraph(Edge[] edgesArr)
+	public WeightedDiGraph(Arrow[] arrowsArr)
 	{
-		edges = new List<Edge>(edgesArr);
-	}
+		arrows = new List<Arrow>();
 
-	public List<Edge> GetEdges()
-	{
-		return edges;
-	}
-
-	public List<Edge> GetArrowsFrom(int i)
-	{
-		List<Edge> result = new List<Edge>();
-
-		foreach (Edge e in edges)
+		foreach (Arrow a in arrowsArr)
 		{
-			if (e.GetBegin() == i)
-				result.Add(e);
+			if (!arrows.Contains(a))
+			{
+				arrows.Add(a);
+			}
+		}
+	}
+
+	public WeightedDiGraph(List<Arrow> arrows)
+	{
+		this.arrows = new List<Arrow>();
+
+		foreach (Arrow a in arrows)
+		{
+			if (!arrows.Contains(a))
+			{
+				this.arrows.Add(a);
+			}
+		}
+	}
+
+	public int GetEdgesCount()
+	{
+		return arrows.Count();
+	}
+
+	public List<int> GetVertices()
+	{
+		List<int> vertices = new List<int>();
+
+		foreach (Arrow a in arrows)
+		{
+			if (!vertices.Contains(a.GetBegin()))
+			{
+				vertices.Add(a.GetBegin());
+			}
+
+			if (!vertices.Contains(a.GetEnd()))
+			{
+				vertices.Add(a.GetEnd());
+			}
+		}
+
+		return vertices;
+	}
+
+	public List<int> GetVerticesSorted()
+	{
+		return Sorter.TopologicalSort(this);
+	}
+
+	public List<Arrow> GetArrows()
+	{
+		List<Arrow> result = new List<Arrow>();
+
+		foreach (Arrow a in arrows)
+		{
+			result.Add(new Arrow(a));
 		}
 
 		return result;
 	}
 
-	public List<Edge> GetArrowsTo(int i)
+	public List<Arrow> GetArrowsFrom(int i)
 	{
-		List<Edge> result = new List<Edge>();
+		List<Arrow> result = new List<Arrow>();
 
-		foreach (Edge e in edges)
+		foreach (Arrow a in arrows)
 		{
-			if (e.GetEnd() == i)
-				result.Add(e);
+			if (a.GetBegin() == i)
+			{
+				result.Add(a);
+			}
+		}
+
+		return result;
+	}
+
+	public List<Arrow> GetArrowsTo(int i)
+	{
+		List<Arrow> result = new List<Arrow>();
+
+		foreach (Arrow a in arrows)
+		{
+			if (a.GetEnd() == i)
+			{
+				result.Add(a);
+			}
 		}
 
 		return result;
@@ -101,56 +173,193 @@ struct WeightedDiGraph
 
 	public bool IsAdjacent(int i, int j)
 	{
-		return edges.Contains(new Edge(i, j));
+		return arrows.Contains(new Arrow(i, j));
 	}
 }
 
 
 
 
+class Sorter
+{
+	private static void DFS
+	(
+		int v,
+		WeightedDiGraph G,
+		Dictionary<int, bool> used,
+		List<int> result
+	)
+	{
+		used[v] = true;
+		List<Arrow> outcomingArrows = G.GetArrowsFrom(v);
+
+		foreach (Arrow a in outcomingArrows)
+		{
+			if (!used[a.GetEnd()])
+			{
+				DFS(a.GetEnd(), G, used, result);
+			}
+		}
+
+		result.Add(v);
+	}
+
+	public static List<int> TopologicalSort(WeightedDiGraph G)
+	{
+		List<int> vertices = G.GetVertices();
+		Dictionary<int, bool> used = new Dictionary<int, bool>();
+		List<int> result = new List<int>();
+
+		foreach (int v in vertices)
+		{
+			used[v] = false;
+		}
+
+		result.Clear();
+		foreach (int v in vertices)
+		{
+			if (!used[v])
+			{
+				DFS(v, G, used, result);
+			}
+		}
+		result.Reverse();
+
+		return result;
+	}
+}
+
 class CPM
 {
-/*	public List<Edge> GetCP(WeightedDiGraph G)
+	/*
+		!!! ATTENTION !!!
+
+		It is important for vertices list in this method
+		to be sorted by partial order given by graph arrows.
+		In other words, order provided by Topological Sortion!
+	*/
+	public static List<Arrow> GetCriticalProcesses(WeightedDiGraph G)
 	{
-		List<Edge> earliestStart = new int[G.GetEdges().Count];
-		int[] earliestFinish = new int[G.GetEdges().Count];
+		Dictionary<int, double> earliestStart = new Dictionary<int, double>();
+		List<int> vertices = G.GetVerticesSorted();
 
-		int[] latestStart = new int[G.GetEdges().Count];
-		int[] latestFinish = new int[G.GetEdges().Count];
+		int source = vertices.Min();
+		earliestStart[source] = 0;
 
-		earliestStart[0] = 0;
+		foreach (int v in vertices)
+		{
+			earliestStart[v] = 0;
+		}
+
+		foreach (int v in vertices)
+		{
+			List<Arrow> incomingArrows = G.GetArrowsTo(v);
+
+			foreach (Arrow a in incomingArrows)
+			{
+				double current = earliestStart[a.GetBegin()] + a.GetWeight();
+
+				if (current > earliestStart[a.GetEnd()])
+				{
+					earliestStart[a.GetEnd()] = current;
+				}
+			}
+		}
+
+
+		int sinkVertex = vertices[vertices.Count() - 1];
+		Dictionary<int, double> latestStart = new Dictionary<int, double>();
+
+		foreach (int v in vertices)
+		{
+			latestStart[v] = double.MaxValue;
+		}
+		latestStart[sinkVertex] = earliestStart[sinkVertex];
+
+		vertices.Reverse();
+
+		foreach (int v in vertices)
+		{
+			List<Arrow> outcomingArrows = G.GetArrowsFrom(v);
+
+			foreach (Arrow a in outcomingArrows)
+			{
+				double current = earliestStart[a.GetEnd()] - a.GetWeight();
+
+				if (current < latestStart[v])
+				{
+					latestStart[v] = current;
+				}
+			}
+		}
+
+
+		List<Arrow> criticalProcesses = new List<Arrow>();
+
+		List<Arrow> arrows = G.GetArrows();
+		foreach (Arrow a in arrows)
+		{
+			if
+			(
+				earliestStart[a.GetBegin()] == latestStart[a.GetBegin()] &&
+				earliestStart[a.GetEnd()] == latestStart[a.GetEnd()] &&
+				earliestStart[a.GetEnd()] - earliestStart[a.GetBegin()] == a.GetWeight() &&
+				latestStart[a.GetEnd()] - latestStart[a.GetBegin()] == a.GetWeight()
+			)
+			{
+				criticalProcesses.Add(a);
+			}
+		}
+
+		return criticalProcesses;
+	}
+
+	/* TODO
+	public List<Arrow> GetCriticalPath(WeightedDiGraph G)
+	{
+		List<Arrow> criticalProcesses = GetCriticalProcesses(G);
+		
 	}*/
 }
 
 
+class GraphReader
+{
+	public static WeightedDiGraph read(string file)
+	{
+		string[] lines = System.IO.File.ReadAllLines(file);
+		Arrow[] arrows = new Arrow[lines.Count()];
+
+		for (int i = 0; i < lines.Count(); i++)
+		{
+			string[] words = lines[i].Split(' ');
+			arrows[i] = new Arrow
+			(
+				Int32.Parse(words[0]),
+				Int32.Parse(words[1]),
+				double.Parse(words[2])
+			);
+		}
+
+		return new WeightedDiGraph(arrows);
+	}
+}
 
 
 class CPMTester
 {
 	static void Main(string[] args)
 	{
-		WeightedDiGraph G_1 = new WeightedDiGraph(
-			new Edge[] {
-				new Edge(1, 2, 3),
-				new Edge(1, 3, 2),
-				new Edge(1, 5, 3),
-				new Edge(1, 8, 4),
+		WeightedDiGraph G_1 = GraphReader.read("network_example_2");
+		List<Arrow> criticalPath = CPM.GetCriticalProcesses(G_1);
 
-				new Edge(2, 3),
-				new Edge(3, 4, 2),
-				new Edge(4, 6, 2),
+		double summ = 0;
+		foreach (Arrow a in criticalPath)
+		{
+			Console.WriteLine(a);
+			summ += a.GetWeight();
+		}
 
-				new Edge(6, 7, 2),
-				new Edge(5, 7, 1),
-
-				new Edge(7, 8, 2),
-
-				new Edge(8, 9, 4)
-			}
-		);
-
-		G_1.GetEdges().Add(new Edge(1, 2, 4));
-
-		System.Console.Write(G_1.GetEdges()[0]);
+		Console.WriteLine("summ = " + summ);
 	}
 }
