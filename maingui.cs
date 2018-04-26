6749c9897ef567ac959c4152328d3eff2f75b51c
bewrrrie <silverr41@gmail.com>
2018-04-27 02:41:22 +0600

    GUI & things

--- maingui.cs
+++ maingui.cs
@@ -1,20 +1,200 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Threading;
+
+using netplanlib;
 
 namespace App
 {
-	public class Simple : Form
+	public class Window : Form
 	{
-		public Simple()
+		private int WIDTH = 1098;
+		private int HEIGHT = 640;
+
+		private int TOP_MARGIN = 100;
+		private int LEFT_MARGIN = 50;
+		private int TEXT_BOX_WIDTH = 50;
+		private int TEXT_BOX_HEIGHT = 23;
+
+		private const string TITLE = "Network Planning";
+
+		private int vertices = 20;
+		private List<TextBox> matrix;
+		private WeightedDiGraph G;
+		private bool loaded = false;
+
+		public Window()
 		{
-			Text = "Simple";
-			Size = new Size(250, 200);
-			CenterToScreen();
+			InitializeFields();
+			InitializeComponent();
+			this.FormBorderStyle = FormBorderStyle.FixedSingle;
 		}
 
-		static public void Main()
+		private void InitializeFields()
 		{
-			Application.Run(new Simple());
+			this.matrix = new List<TextBox>();
+			this.SuspendLayout();
+			this.ClientSize = new System.Drawing.Size(WIDTH, HEIGHT);
+			this.Text = TITLE;
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+
+		private void InitializeComponent()
+		{
+			matrix.Clear();
+
+			Label verticesNumberField = new Label();
+			verticesNumberField.Text = " = " + vertices;
+			verticesNumberField.Left = 3 * LEFT_MARGIN / 2 + TEXT_BOX_WIDTH;
+			verticesNumberField.Top = TOP_MARGIN / 4 + 4;
+			verticesNumberField.Font = new Font("Georgia", 8);
+			verticesNumberField.SetBounds
+			(
+				verticesNumberField.Location.X, verticesNumberField.Location.Y,
+				TEXT_BOX_WIDTH, TEXT_BOX_HEIGHT
+			);
+			this.Controls.Add(verticesNumberField);
+
+			Label label = new Label();
+			label.Left = LEFT_MARGIN;
+			label.Top = TOP_MARGIN / 4;
+			label.Font = new Font("Georgia", 8);
+			label.Text = "Maximal number\nof vertices";
+			label.SetBounds(label.Location.X, label.Location.Y, 128, 37);
+			this.Controls.Add(label);
+
+			for (int i = 0; i < vertices * vertices; i++)
+				AddTextBox();
+
+			Button SolveButton = new Button();
+			SolveButton.Height = 40;
+			SolveButton.Width = 300;
+			SolveButton.Left = WIDTH / 2 - 150;
+			SolveButton.Top = (vertices + 5) * TEXT_BOX_HEIGHT;
+			SolveButton.Text = "Solve";
+			SolveButton.Font = new Font("Georgia", 16);
+			SolveButton.Click += new EventHandler(SolveButton_Click);
+			Controls.Add(SolveButton);
+
+			Button loadButton = new Button();
+			loadButton.Left = 13 * LEFT_MARGIN;
+			loadButton.Top = 2 * TOP_MARGIN / 4 ;
+			loadButton.Font = new Font("Georgia", 10);
+			loadButton.Click += new EventHandler(LoadButton_Click);
+			loadButton.Text = "Load from file";
+			loadButton.SetBounds(loadButton.Location.X, loadButton.Location.Y, 128, 22);
+			this.Controls.Add(loadButton);
+
+			Label pathLabel = new Label();
+			pathLabel.Left = 12 * LEFT_MARGIN;
+			pathLabel.Top = TOP_MARGIN / 4;
+			pathLabel.Font = new Font("Georgia", 8);
+			pathLabel.Text = "Path: ";
+			pathLabel.SetBounds(pathLabel.Location.X, pathLabel.Location.Y, 50, 37);
+			this.Controls.Add(pathLabel);
+			
+			TextBox pathBox = new TextBox();
+			pathBox.Left = 13 * LEFT_MARGIN;
+			pathBox.Top = TOP_MARGIN / 4;
+			pathBox.Font = new Font("Georgia", 8);
+			pathBox.Name = "path";
+			pathBox.SetBounds(pathBox.Location.X, pathBox.Location.Y, 275, 20);
+			this.Controls.Add(pathBox);
+		}
+
+		private void SolveButton_Click(object sender, EventArgs e)
+		{
+			if (!loaded)
+			{
+				List<Arrow> arrows = new List<Arrow>();
+
+				for (int i = 1; i <= vertices; i++)
+					for (int j = 1; j <= vertices; j++)
+					{
+						double weight = Convert.ToDouble(Controls.Find(i + "," + j, true)[0].Text);
+
+						if (weight >= 0)
+							arrows.Add(new Arrow(i, j, weight));
+					}
+
+				G = new WeightedDiGraph(arrows);
+			}
+
+			List<Arrow> cp = CPM.GetCriticalPath(G);
+			string answer = "Critical path: " + cp[0].GetBegin();
+
+			double summWeight = 0;
+			foreach (Arrow a in cp)
+			{
+				summWeight += a.GetWeight();
+				answer += ", " + a.GetEnd();
+			}
+
+			MessageBox.Show(answer + "\nWieght = " + summWeight);
+		}
+
+		private void LoadButton_Click(object sender, EventArgs e)
+		{
+			G = GraphReader.read(Controls.Find("path", true)[0].Text);
+			int n = G.GetVertices().Count();
+
+			for (int i = 1; i <= n; i++)
+				for (int j = 1; j <= n; j++)
+					Controls.Find(i + "," + j, true)[0].Text = G.GetArrowWeight(i, j) + "";
+
+			loaded = true;
+		}
+
+		private void AddTextBox()
+		{
+			TextBox box = new System.Windows.Forms.TextBox();
+
+			box.Text = "-1";
+			box.AcceptsReturn = true;
+			box.AcceptsTab = true;
+			box.Name = (matrix.Count() / vertices + 1) + "," + (matrix.Count() % vertices + 1);
+			box.Left = LEFT_MARGIN + TEXT_BOX_WIDTH * (matrix.Count() % vertices);
+			box.Top = TOP_MARGIN + TEXT_BOX_HEIGHT * (matrix.Count() / vertices);
+			box.SetBounds(box.Location.X, box.Location.Y, TEXT_BOX_WIDTH, TEXT_BOX_HEIGHT);
+			box.MaxLength = 10;
+
+			if (matrix.Count() < vertices)
+			{
+				Label label = new Label();
+
+				label.Text = 1 + matrix.Count() + "";
+				label.Left = box.Left + 5;
+				label.Top = TOP_MARGIN - 15;
+				label.Font = new Font("Georgia", 8);
+				label.SetBounds(label.Location.X, label.Location.Y, 15, 15);
+				this.Controls.Add(label);
+			}
+			if ((matrix.Count() - 1) % vertices == 0)
+			{
+				Label label = new Label();
+				label.Text = 1 + matrix.Count() / vertices + "";
+				label.Left = LEFT_MARGIN - 15;
+				label.Top = box.Top;
+				label.Font = new Font("Georgia", 8);
+				label.SetBounds(label.Location.X, label.Location.Y, 15, 15);
+				this.Controls.Add(label);
+			}
+
+			this.Controls.Add(box);
+			this.matrix.Add(box);
+		}
+
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Window());
+		}
+
 	}
 }
--- netplanlib.cs
+++ netplanlib.cs
@@ -4,373 +4,385 @@ using System.Collections.Generic;
 using System.Linq;
 
 
-struct Arrow {
-	private int begin;
-	private int end;
-	private double weight;
+namespace netplanlib {
+	public struct Arrow {
+		private int begin;
+		private int end;
+		private double weight;
 
-	public Arrow(Arrow e)
-	{
-		this.begin = e.begin;
-		this.end = e.end;
-		this.weight = e.weight;
-	}
-
-	public Arrow(int begin, int end, double weight)
-	{
-		this.begin = begin;
-		this.end = end;
-		this.weight = weight;
-	}
-
-	public Arrow(int begin, int end)
-	{
-		this.begin = begin;
-		this.end = end;
-		this.weight = 0;
-	}
-
-	public int GetBegin()
-	{
-		return begin;
-	}
-
-	public int GetEnd()
-	{
-		return end;
-	}
-
-	public double GetWeight()
-	{
-		return weight;
-	}
-
-	public override string ToString()
-	{
-		return "(" + begin + ", " + end + ", weight=" + weight + ")";
-	}
-
-	public override bool Equals(object obj) 
-	{
-		if (!(obj is Arrow))
-			return false;
-
-		Arrow a = (Arrow) obj;
-		return a.begin == this.begin && a.end == this.end;
-	}
-
-	public override int GetHashCode()
-	{
-		return begin + end * 317;
-	}
-}
-
-
-struct WeightedDiGraph
-{
-	private List<Arrow> arrows;
-
-	public WeightedDiGraph(Arrow[] arrowsArr)
-	{
-		arrows = new List<Arrow>();
-
-		foreach (Arrow a in arrowsArr)
+		public Arrow(Arrow e)
 		{
-			arrows.Add(new Arrow(a));
+			this.begin = e.begin;
+			this.end = e.end;
+			this.weight = e.weight;
+		}
+
+		public Arrow(int begin, int end, double weight)
+		{
+			this.begin = begin;
+			this.end = end;
+			this.weight = weight;
+		}
+
+		public Arrow(int begin, int end)
+		{
+			this.begin = begin;
+			this.end = end;
+			this.weight = 0;
+		}
+
+		public int GetBegin()
+		{
+			return begin;
+		}
+
+		public int GetEnd()
+		{
+			return end;
+		}
+
+		public double GetWeight()
+		{
+			return weight;
+		}
+
+		public override string ToString()
+		{
+			return "(" + begin + ", " + end + ", weight=" + weight + ")";
+		}
+
+		public override bool Equals(object obj) 
+		{
+			if (!(obj is Arrow))
+				return false;
+
+			Arrow a = (Arrow) obj;
+			return a.begin == this.begin && a.end == this.end;
+		}
+
+		public override int GetHashCode()
+		{
+			return begin + end * 317;
 		}
 	}
 
-	public WeightedDiGraph(List<Arrow> arrows)
-	{
-		this.arrows = new List<Arrow>();
 
-		foreach (Arrow a in arrows)
+	public struct WeightedDiGraph
+	{
+		private List<Arrow> arrows;
+
+		public WeightedDiGraph(Arrow[] arrowsArr)
 		{
-			this.arrows.Add(new Arrow(a));
-		}
-	}
+			arrows = new List<Arrow>();
 
-	public int GetEdgesCount()
-	{
-		return arrows.Count();
-	}
-
-	public List<int> GetVertices()
-	{
-		List<int> vertices = new List<int>();
-
-		foreach (Arrow a in arrows)
-		{
-			if (!vertices.Contains(a.GetBegin()))
+			foreach (Arrow a in arrowsArr)
 			{
-				vertices.Add(a.GetBegin());
-			}
-
-			if (!vertices.Contains(a.GetEnd()))
-			{
-				vertices.Add(a.GetEnd());
+				arrows.Add(new Arrow(a));
 			}
 		}
 
-		return vertices;
-	}
-
-	public List<int> GetVerticesSorted()
-	{
-		return Sorter.TopologicalSort(this);
-	}
-
-	public List<Arrow> GetArrows()
-	{
-		List<Arrow> result = new List<Arrow>();
-
-		foreach (Arrow a in arrows)
+		public WeightedDiGraph(List<Arrow> arrows)
 		{
-			result.Add(new Arrow(a));
-		}
+			this.arrows = new List<Arrow>();
 
-		return result;
-	}
-
-	public List<Arrow> GetArrowsFrom(int i)
-	{
-		List<Arrow> result = new List<Arrow>();
-
-		foreach (Arrow a in arrows)
-		{
-			if (a.GetBegin() == i)
+			foreach (Arrow a in arrows)
 			{
-				result.Add(new Arrow(a));
+				this.arrows.Add(new Arrow(a));
 			}
 		}
 
-		return result;
-	}
-
-	public List<Arrow> GetArrowsTo(int i)
-	{
-		List<Arrow> result = new List<Arrow>();
-
-		foreach (Arrow a in arrows)
+		public int GetArrowsCount()
 		{
-			if (a.GetEnd() == i)
+			return arrows.Count();
+		}
+
+		public List<int> GetVertices()
+		{
+			List<int> vertices = new List<int>();
+
+			foreach (Arrow a in arrows)
 			{
-				result.Add(new Arrow(a));
-			}
-		}
-
-		return result;
-	}
-
-	public bool IsAdjacent(int i, int j)
-	{
-		return arrows.Contains(new Arrow(i, j));
-	}
-}
-
-
-class Sorter
-{
-	private static void DFS
-	(
-		int v,
-		WeightedDiGraph G,
-		Dictionary<int, bool> used,
-		List<int> result
-	)
-	{
-		used[v] = true;
-		List<Arrow> outcomingArrows = G.GetArrowsFrom(v);
-
-		foreach (Arrow a in outcomingArrows)
-		{
-			if (!used[a.GetEnd()])
-			{
-				DFS(a.GetEnd(), G, used, result);
-			}
-		}
-
-		result.Add(v);
-	}
-
-	public static List<int> TopologicalSort(WeightedDiGraph G)
-	{
-		List<int> vertices = G.GetVertices();
-		Dictionary<int, bool> used = new Dictionary<int, bool>();
-		List<int> result = new List<int>();
-
-		foreach (int v in vertices)
-		{
-			used[v] = false;
-		}
-
-		result.Clear();
-		foreach (int v in vertices)
-		{
-			if (!used[v])
-			{
-				DFS(v, G, used, result);
-			}
-		}
-		result.Reverse();
-
-		return result;
-	}
-
-	public static int GetTopologicalMin(WeightedDiGraph G)
-	{
-		return TopologicalSort(G)[0];
-	}
-
-	public static int GetTopologicalMax(WeightedDiGraph G)
-	{
-		List<int> sorted = TopologicalSort(G);
-		return sorted[sorted.Count() - 1];
-	}
-}
-
-class CPM
-{
-	public static List<Arrow> GetCriticalProcesses(WeightedDiGraph G)
-	{
-		Dictionary<int, double> earliestStart = new Dictionary<int, double>();
-		List<int> vertices = G.GetVerticesSorted();
-
-		int source = vertices.Min();
-		earliestStart[source] = 0;
-
-		foreach (int v in vertices)
-		{
-			earliestStart[v] = 0;
-		}
-
-		foreach (int v in vertices)
-		{
-			List<Arrow> incomingArrows = G.GetArrowsTo(v);
-
-			foreach (Arrow a in incomingArrows)
-			{
-				double current = earliestStart[a.GetBegin()] + a.GetWeight();
-
-				if (current > earliestStart[a.GetEnd()])
+				if (!vertices.Contains(a.GetBegin()))
 				{
-					earliestStart[a.GetEnd()] = current;
+					vertices.Add(a.GetBegin());
+				}
+
+				if (!vertices.Contains(a.GetEnd()))
+				{
+					vertices.Add(a.GetEnd());
 				}
 			}
+
+			return vertices;
 		}
 
-
-		int sinkVertex = vertices[vertices.Count() - 1];
-		Dictionary<int, double> latestStart = new Dictionary<int, double>();
-
-		foreach (int v in vertices)
+		public List<int> GetVerticesSorted()
 		{
-			latestStart[v] = double.MaxValue;
+			return Sorter.TopologicalSort(this);
 		}
-		latestStart[sinkVertex] = earliestStart[sinkVertex];
 
-		vertices.Reverse();
-
-		foreach (int v in vertices)
+		public List<Arrow> GetArrows()
 		{
+			List<Arrow> result = new List<Arrow>();
+
+			foreach (Arrow a in arrows)
+			{
+				result.Add(new Arrow(a));
+			}
+
+			return result;
+		}
+
+		public List<Arrow> GetArrowsFrom(int i)
+		{
+			List<Arrow> result = new List<Arrow>();
+
+			foreach (Arrow a in arrows)
+			{
+				if (a.GetBegin() == i)
+				{
+					result.Add(new Arrow(a));
+				}
+			}
+
+			return result;
+		}
+
+		public List<Arrow> GetArrowsTo(int i)
+		{
+			List<Arrow> result = new List<Arrow>();
+
+			foreach (Arrow a in arrows)
+			{
+				if (a.GetEnd() == i)
+				{
+					result.Add(new Arrow(a));
+				}
+			}
+
+			return result;
+		}
+
+		public double GetArrowWeight(int i, int j)
+		{
+			List<Arrow> arrows = GetArrowsFrom(i);
+			foreach (Arrow a in arrows)
+				if (a.GetEnd() == j)
+					return a.GetWeight();
+
+			return -1;
+		}
+
+		public bool IsAdjacent(int i, int j)
+		{
+			return arrows.Contains(new Arrow(i, j));
+		}
+	}
+
+
+	public class Sorter
+	{
+		private static void DFS
+		(
+			int v,
+			WeightedDiGraph G,
+			Dictionary<int, bool> used,
+			List<int> result
+		)
+		{
+			used[v] = true;
 			List<Arrow> outcomingArrows = G.GetArrowsFrom(v);
 
 			foreach (Arrow a in outcomingArrows)
 			{
-				double current = earliestStart[a.GetEnd()] - a.GetWeight();
-
-				if (current < latestStart[v])
+				if (!used[a.GetEnd()])
 				{
-					latestStart[v] = current;
+					DFS(a.GetEnd(), G, used, result);
 				}
 			}
+
+			result.Add(v);
 		}
 
-
-		List<Arrow> criticalProcesses = new List<Arrow>();
-
-		List<Arrow> arrows = G.GetArrows();
-		foreach (Arrow a in arrows)
+		public static List<int> TopologicalSort(WeightedDiGraph G)
 		{
-			if
-			(
-				earliestStart[a.GetBegin()] == latestStart[a.GetBegin()] &&
-				earliestStart[a.GetEnd()] == latestStart[a.GetEnd()] &&
-				earliestStart[a.GetEnd()] - earliestStart[a.GetBegin()] == a.GetWeight() &&
-				latestStart[a.GetEnd()] - latestStart[a.GetBegin()] == a.GetWeight()
-			)
+			List<int> vertices = G.GetVertices();
+			Dictionary<int, bool> used = new Dictionary<int, bool>();
+			List<int> result = new List<int>();
+
+			foreach (int v in vertices)
 			{
-				criticalProcesses.Add(new Arrow(a));
+				used[v] = false;
 			}
-		}
 
-		return criticalProcesses;
-	}
-
-
-	public static List<Arrow> GetCriticalPath(WeightedDiGraph G)
-	{
-		WeightedDiGraph subgraph = new WeightedDiGraph(GetCriticalProcesses(G));
-		List<Arrow> outcomingArrows = subgraph.GetArrowsFrom(Sorter.GetTopologicalMin(G));
-		List<Arrow> resultPath = new List<Arrow>();
-
-		foreach (Arrow a in outcomingArrows)
-		{
-			DFS(subgraph, a, resultPath, Sorter.GetTopologicalMax(G));
-		}
-
-		resultPath.Reverse();
-		return resultPath;
-	}
-
-	private static bool DFS
-	(
-		WeightedDiGraph G,
-		Arrow current,
-		List<Arrow> resultPath,
-		int sink
-	)
-	{
-		if (current.GetEnd() == sink)
-		{
-			resultPath.Add(new Arrow(current));
-			return true;
-		}
-
-		List<Arrow> outcomingArrows = G.GetArrowsFrom(current.GetEnd());
-
-		foreach (Arrow a in outcomingArrows)
-		{
-			if (DFS(G, a, resultPath, sink))
+			result.Clear();
+			foreach (int v in vertices)
 			{
-				resultPath.Add(current);
+				if (!used[v])
+				{
+					DFS(v, G, used, result);
+				}
+			}
+			result.Reverse();
+
+			return result;
+		}
+
+		public static int GetTopologicalMin(WeightedDiGraph G)
+		{
+			return TopologicalSort(G)[0];
+		}
+
+		public static int GetTopologicalMax(WeightedDiGraph G)
+		{
+			List<int> sorted = TopologicalSort(G);
+			return sorted[sorted.Count() - 1];
+		}
+	}
+
+	public class CPM
+	{
+		public static List<Arrow> GetCriticalProcesses(WeightedDiGraph G)
+		{
+			Dictionary<int, double> earliestStart = new Dictionary<int, double>();
+			List<int> vertices = G.GetVerticesSorted();
+
+			int source = vertices.Min();
+			earliestStart[source] = 0;
+
+			foreach (int v in vertices)
+			{
+				earliestStart[v] = 0;
+			}
+
+			foreach (int v in vertices)
+			{
+				List<Arrow> incomingArrows = G.GetArrowsTo(v);
+
+				foreach (Arrow a in incomingArrows)
+				{
+					double current = earliestStart[a.GetBegin()] + a.GetWeight();
+
+					if (current > earliestStart[a.GetEnd()])
+					{
+						earliestStart[a.GetEnd()] = current;
+					}
+				}
+			}
+
+
+			int sinkVertex = vertices[vertices.Count() - 1];
+			Dictionary<int, double> latestStart = new Dictionary<int, double>();
+
+			foreach (int v in vertices)
+			{
+				latestStart[v] = double.MaxValue;
+			}
+			latestStart[sinkVertex] = earliestStart[sinkVertex];
+
+			vertices.Reverse();
+
+			foreach (int v in vertices)
+			{
+				List<Arrow> outcomingArrows = G.GetArrowsFrom(v);
+
+				foreach (Arrow a in outcomingArrows)
+				{
+					double current = earliestStart[a.GetEnd()] - a.GetWeight();
+
+					if (current < latestStart[v])
+					{
+						latestStart[v] = current;
+					}
+				}
+			}
+
+
+			List<Arrow> criticalProcesses = new List<Arrow>();
+
+			List<Arrow> arrows = G.GetArrows();
+			foreach (Arrow a in arrows)
+			{
+				if
+				(
+					earliestStart[a.GetBegin()] == latestStart[a.GetBegin()] &&
+					earliestStart[a.GetEnd()] == latestStart[a.GetEnd()] &&
+					earliestStart[a.GetEnd()] - earliestStart[a.GetBegin()] == a.GetWeight() &&
+					latestStart[a.GetEnd()] - latestStart[a.GetBegin()] == a.GetWeight()
+				)
+				{
+					criticalProcesses.Add(new Arrow(a));
+				}
+			}
+
+			return criticalProcesses;
+		}
+
+
+		public static List<Arrow> GetCriticalPath(WeightedDiGraph G)
+		{
+			WeightedDiGraph subgraph = new WeightedDiGraph(GetCriticalProcesses(G));
+			List<Arrow> outcomingArrows = subgraph.GetArrowsFrom(Sorter.GetTopologicalMin(G));
+			List<Arrow> resultPath = new List<Arrow>();
+
+			foreach (Arrow a in outcomingArrows)
+			{
+				DFS(subgraph, a, resultPath, Sorter.GetTopologicalMax(G));
+			}
+
+			resultPath.Reverse();
+			return resultPath;
+		}
+
+		private static bool DFS
+		(
+			WeightedDiGraph G,
+			Arrow current,
+			List<Arrow> resultPath,
+			int sink
+		)
+		{
+			if (current.GetEnd() == sink)
+			{
+				resultPath.Add(new Arrow(current));
 				return true;
 			}
-		}
 
-		return false;
+			List<Arrow> outcomingArrows = G.GetArrowsFrom(current.GetEnd());
+
+			foreach (Arrow a in outcomingArrows)
+			{
+				if (DFS(G, a, resultPath, sink))
+				{
+					resultPath.Add(current);
+					return true;
+				}
+			}
+
+			return false;
+		}
 	}
-}
 
 
-class GraphReader
-{
-	public static WeightedDiGraph read(string file)
+	public class GraphReader
 	{
-		string[] lines = System.IO.File.ReadAllLines(file);
-		Arrow[] arrows = new Arrow[lines.Count()];
-
-		for (int i = 0; i < lines.Count(); i++)
+		public static WeightedDiGraph read(string file)
 		{
-			string[] words = lines[i].Split(' ');
-			arrows[i] = new Arrow
-			(
-				Int32.Parse(words[0]),
-				Int32.Parse(words[1]),
-				double.Parse(words[2])
-			);
-		}
+			string[] lines = System.IO.File.ReadAllLines(file);
+			Arrow[] arrows = new Arrow[lines.Count()];
 
-		return new WeightedDiGraph(arrows);
+			for (int i = 0; i < lines.Count(); i++)
+			{
+				string[] words = lines[i].Split(' ');
+				arrows[i] = new Arrow
+				(
+					Int32.Parse(words[0]),
+					Int32.Parse(words[1]),
+					double.Parse(words[2])
+				);
+			}
+
+			return new WeightedDiGraph(arrows);
+		}
 	}
 }
